a5130f76653b03869d9c4ed752bb1134627d5e8b
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-10-31 15:50:40 +0530

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,22 @@
+public static void bubbleSort(int[] arr, int n) {
+     
+     for (int i = 1; i < n; i++) {
+         boolean swapped = false;
+         
+         for (int j = 0; j < n - i; j++) {
+             
+             if (arr[j] > arr[j + 1]) {
+                 int temp = arr[j];
+                 arr[j] = arr[j + 1];
+                 arr[j + 1] = temp;
+                 
+                 swapped = true;
+             }
+         }
+         
+         if (swapped == false) {
+             break;
+         }
+     }
+ }
+
